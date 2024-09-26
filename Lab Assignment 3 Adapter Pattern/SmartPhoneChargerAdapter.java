d06b8550c1133f3b5cf83b9ef4cba4186e870bfe
Arthur Gesmundo <niahstic@gmail.com>
2024-09-26 23:00:22 +0800

    Add files via upload
    comment1

---
+++ Lab Assignment 3 Adapter Pattern/SmartPhoneChargerAdapter.java
@@ -0,0 +1,14 @@
+public class SmartPhoneChargerAdapter implements PowerOutlet{
+    private SmartPhoneCharger smartPhoneCharger;
+
+    public SmartPhoneChargerAdapter(SmartPhoneCharger smartPhoneCharger){
+        this.smartPhoneCharger = smartPhoneCharger;
+    }
+
+
+    @Override
+    public String plugIn() {
+        return smartPhoneCharger.chargePhone();
+    }
+    
+}
